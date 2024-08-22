470936f371a075342a920e484a01c60ba5e7df9b
Ahmed Bouzaffour <ahmedbouzaffour2002@gmail.com>
2024-08-22 11:07:40 -0600

    Player moves correctly on a 60FPS

--- GamePanel.java
+++ GamePanel.java
@@ -39,6 +39,7 @@ public class GamePanel extends JPanel  implements Runnable{
         gameThread = new Thread(this);
         gameThread.start();
     }
+   /*
     @Override
     public void run() {
 
@@ -54,12 +55,53 @@ public class GamePanel extends JPanel  implements Runnable{
             try {
                 double remainingTime = nextDrawTime - System.nanoTime();
                 remainingTime = remainingTime / 1000000;
+
+                if(remainingTime <0){
+                    remainingTime = 0;
+                }
+
                Thread.sleep((long) remainingTime);
+
+                nextDrawTime +=drawInterval;
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
-   }
+   }*/
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        long timer  = 0;
+        int drawCount = 0;
+
+        while (gameThread != null)
+        {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) /drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta>1)
+            {
+                update();
+                repaint();
+                delta--;
+                drawCount ++;
+            }
+            if (timer >= 1000000000)
+            {
+                System.out.println("FPS:"+drawCount);
+                drawCount = 0;
+                timer=0;
+            }
+        }
+    }
+
     public void update(){
         if(keyH.upPressed)
         {
