5778a5935fb5ac6f18198fe7b84f236d8d01255e
Ahmed Bouzaffour <ahmedbouzaffour2002@gmail.com>
2024-08-22 09:22:50 -0600

    GamePanel

---
+++ GamePanel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+    // Screen setting
+    final  int originalTileSize = 16; // 16*16 Tile
+    final it scale = 3; // it looks 16 * 3 = 46
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+
+}
